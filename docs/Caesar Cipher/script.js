21cf385dcf1d6de5a844ffc3d2dc8ab7a0bfc7cd
carweaver <carweaver@packer.edu>
2018-04-24 10:39:08 -0400

    caesar cipher

--- docs/Caesar Cipher/script.js
+++ docs/Caesar Cipher/script.js
@@ -31,11 +31,4 @@ function encode() {
 	}
 }
 
-/* Getting the ASCII value from a character in a string:
-  var x = "Hello World";        Start with a string variable
-  var xVal = x.charCodeAt(0)   Call the charCodeAt method on the string object
-                                The parameter it takes is a position in the string
-                                e.g. 0 is H, 1 is e, 2 is l, etc
-                                It will return the ASCII value of the character in that position
-  console.log(xVal)             Prints: 72 to the console
-*/
+// My code works for the most part except I couldn't figure out how to use modulo.
